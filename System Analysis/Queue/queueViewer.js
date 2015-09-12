6a49a5ac2d1e8720b0834c87d46c5e4510784745
Nikolay1995 <nikkomyk@gmail.com>
2015-09-12 15:22:38 +0300

    Queue working, with styles, without animation

---
+++ System Analysis/Queue/queueViewer.js
@@ -0,0 +1,17 @@
+var queueView = [];
+
+var add = function( q, x) {
+    q[q.length] = x;
+}
+
+var remove = function(q) {
+    return q.splice(0,1);
+}
+
+var isEmpty = function(q) {
+    if (q.length == 0) {
+        return true;    
+    } else {
+        return false;
+    }
+}
